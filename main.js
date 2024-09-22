423281a31c71545bb66131fb0ff6f4ab043110df
AsheedEliyangod <168527984+AsheedEliyangod@users.noreply.github.com>
2024-09-22 11:32:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.querySelector('.loader');
+    loader.classList.remove('visible');
+    document.body.style.overflow = 'auto';
+});
