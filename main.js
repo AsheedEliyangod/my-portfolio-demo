85dfbf036f81870a7cc7675a99d8ce0ef63d1c60
AsheedEliyangod <168527984+AsheedEliyangod@users.noreply.github.com>
2026-03-17 23:42:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function (gltf) {
     model.position.sub(center)
 
     /* ✅ SCALE FIX */
-    model.scale.set(0.01, 0.01, 0.01)
+    model.scale.set(0.0001, 0.0001, 0.0001)
 
     /* ✅ LOOK AT CENTER */
     camera.lookAt(0, 0, 0)
